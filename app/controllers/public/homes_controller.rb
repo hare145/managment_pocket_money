ccfe8306a6b3e963a68f36b84ae95d8bd600bfc0
hare145 <130465393+hare145@users.noreply.github.com>
2024-01-06 12:07:27 +0000

    top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < 
   
   def top
-    
+    @user_income = UserIncome.where(created_at: @month.in_time_zone.all_month)
+    @user_expenditure = UserExpenditure.where(created_at: @month.in_time_zone.all_monthh) 
   end
 end
