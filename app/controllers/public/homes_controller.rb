b63ebb5efd55b98b892bd86125cd1a4bf8004b2c
hare145 <130465393+hare145@users.noreply.github.com>
2024-01-14 14:51:54 +0000

    error解消途中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
-class Public::HomesController < 
+class Public::HomesController < ApplicationController
   
   def top
+    @month = params[:month] ? Date.parse(params[:month]) : Date.today 
     @user_income = UserIncome.where(created_at: @month.in_time_zone.all_month)
-    @user_expenditure = UserExpenditure.where(created_at: @month.in_time_zone.all_monthh) 
+    @user_expenditure = UserExpenditure.where(created_at: @month.in_time_zone.all_month) 
   end
 end
