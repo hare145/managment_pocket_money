cbb605841da752e747f8bf9465b16003a2ed318f
hare145 <130465393+hare145@users.noreply.github.com>
2024-01-17 13:34:01 +0000

    error解消途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,15 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "chartkick/chart.js"
-
-import 'bootstrap'
-import 'bootstrap/dist/js/bootstrap'
-import '../stylesheets/application';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "channels"
+import "chartkick/chart.js"
+
+require("jquery")
+
+import 'bootstrap'
+import '../stylesheets/application';
