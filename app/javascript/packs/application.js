a8c9971a3a584a5955b31033433a5ed3a79157f1
hare145 <130465393+hare145@users.noreply.github.com>
2024-01-20 12:45:08 +0000

    bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
+import "bootstrap";
+import "../stylesheets/application.scss";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
@@ -16,5 +19,3 @@ import "chartkick/chart.js"
 
 require("jquery")
 
-import 'bootstrap'
-import '../stylesheets/application';
